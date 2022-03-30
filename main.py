910038ca781909dcab770f907a74b995287c675b
Stephanie Elluh <99142577+StephanieAda@users.noreply.github.com>
2022-03-30 23:27:10 +0100

    Exception Handling

--- main.py
+++ main.py
@@ -6,13 +6,20 @@ nato_data = pandas.read_csv('nato_phonetic_alphabet.csv')
 nato_dict = {row.letter: row.code for (index, row) in nato_data.iterrows()}
 # print(nato_dict)
 
+start = True
 # Create a list of the phonetic code words from a word that the user inputs.
-user_input = input("Enter a word? ").upper()
-user_input_list = list(user_input)
-user_list = [nato_dict[item] for item in user_input_list]
-
-print(user_list)
+while start:
+    user_input = input("Enter a word? ").upper()
+    try:
+        user_input_list = list(user_input)
+        user_list = [nato_dict[item] for item in user_input_list]
+        print(user_list)
+        start = False
+    except KeyError:
+        print("Sorry, we only accept alphabets please.")
 
 # ...........OR........ Which is not in order by the way
 # user_input_dict = [value for (key, value) in nato_dict.items() if key in user_input_list]
 # print(user_input_dict)
+
+# Exception Handling
